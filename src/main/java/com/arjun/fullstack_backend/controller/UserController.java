9fadf250e4f2404e7ee0818078b84ba657f87bdc
Mampenda <amalie.mampenda@gmail.com>
2024-09-06 20:10:50 +0200

    Started connecting back-end to front-end

--- src/main/java/com/arjun/fullstack_backend/controller/UserController.java
+++ src/main/java/com/arjun/fullstack_backend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.arjun.fullstack_backend.controller;
 import com.arjun.fullstack_backend.model.User;
 import com.arjun.fullstack_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000") /*Connect to front-end*/
 public class UserController {
 
     @Autowired
--- src/main/java/com/arjun/fullstack_backend/model/User.java
+++ src/main/java/com/arjun/fullstack_backend/model/User.java
@@ -3,6 +3,10 @@ package com.arjun.fullstack_backend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+//import jakarta.persistence.OneToMany;
+
+//import java.util.ArrayList;
+//import java.util.List;
 
 @Entity
 public class User {
@@ -11,7 +15,11 @@ public class User {
     @GeneratedValue
     private Long id;
 
+//    @OneToMany
+//    private List<E> polls = new ArrayList<E>();
+
     //User fields
+    private String name;
     private String username;
     private String email;
     private String password;
@@ -19,6 +27,9 @@ public class User {
     //Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
@@ -28,4 +39,7 @@ public class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+//    public String getPolls() { return polls; }
+//    public void setPolls(List<E> polls) { this.polls = polls; }
 }
