18d264e7995ffff79b2445a0120775764e0ff1a1
Mampenda <amalie.mampenda@gmail.com>
2024-09-06 16:17:25 +0200

    started react frontend

--- src/main/java/com/arjun/fullstack_backend/FullstackBackendApplication.java
+++ src/main/java/com/arjun/fullstack_backend/FullstackBackendApplication.java
@@ -2,12 +2,19 @@ package com.arjun.fullstack_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class FullstackBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackBackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
